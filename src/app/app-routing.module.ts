cfb02c4f4f203ae947a645703ad7545ce90e4d77
Miten Chauhan <miten.chauhan@safer.me>
2019-06-02 19:37:57 +1200

    Create auth guard and loging pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'shopping', loadChildren: './pages/shopping/shopping.module#ShoppingPageModule' },
+  { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' }
 ];
 @NgModule({
   imports: [
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(): boolean {
+    return true;
+  }
+}
