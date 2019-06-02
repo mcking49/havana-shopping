69f07df796bf7d24ca3e72724df8b1d84b49e82a
Miten Chauhan <miten.chauhan@safer.me>
2019-06-03 00:43:52 +1200

    Always enable service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
     AngularFirestoreModule,
     AngularFireAuthModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js')
   ],
   providers: [
     StatusBar,
