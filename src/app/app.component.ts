66569b6fc846a435bd73e70c6b739ef65283e009
Miten Chauhan <miten.chauhan@safer.me>
2019-06-03 09:39:23 +1200

    Remove header mode ios and listen for sw updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
+    private alertCtrl: AlertController,
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private swUpdate: SwUpdate
   ) {
     this.initializeApp();
+  }
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(async () => {
+        const alert = await this.alertCtrl.create({
+          header: 'App update!',
+          message: 'A new update is available, do you want to download it now?',
+          buttons: [
+            {
+              text: 'No',
+              role: 'cancel',
+              cssClass: 'medium'
+            },
+            {
+              text: 'Yes',
+              handler: () => {
+                window.location.reload();
+              }
+            }
+          ]
+        });
+        await alert.present();
+      });
+    }
   }
 
   initializeApp() {
